87c2ecfc4f718ecf9c056d0a0bae0d456ad2288e
rumeshsmrr <siriwardhanarumesh@gmail.com>
2025-07-21 18:12:57 +0530

    feat: add DateTimePicker for date selection in NotifyDriverScreen; update dependencies

--- app/NotifyDriverScreen.tsx
+++ app/NotifyDriverScreen.tsx
@@ -1,15 +1,16 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import React from "react";
+import DateTimePicker from "@react-native-community/datetimepicker"; // Import the new DatePicker
+import React, { useState } from "react"; // Import useState
 import {
   Image,
+  Platform,
   SafeAreaView,
   ScrollView,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-
 import Header2 from "./Components/header2";
 
 const scrollViewBottomPadding = 24; // Define the padding value
@@ -50,7 +51,21 @@ const selectedChild: Child[] = [
     },
   },
 ];
+
 const NotifyDriverScreen = () => {
+  const [date, setDate] = useState(new Date()); // State to hold the selected date
+  const [showPicker, setShowPicker] = useState(false); // State to control picker visibility
+
+  const onChange = (event: any, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setShowPicker(Platform.OS === "ios"); // On iOS, the picker stays open by default, close it here. On Android, it closes automatically.
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShowPicker(true);
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-light-100 py-9">
       {/* Main scrollable content area */}
@@ -68,61 +83,91 @@ const NotifyDriverScreen = () => {
           {selectedChild.map((child: Child, index: number) => (
             <TouchableOpacity
               key={index}
-              className="flex-col  gap-4 bg-white p-4 rounded-lg shadow"
+              className="flex-col gap-4 bg-white p-4 rounded-lg shadow"
             >
-              <View className="flex-row items-center gap-4">
-                <Image
-                  source={child.image}
-                  style={{ height: 48, width: 48, borderRadius: 24 }}
-                />
-                <View className="flex-1">
-                  <Text className="text-lg font-semibold">{child.name}</Text>
-                  <Text className="text-sm text-blue-700 font-light">
-                    {child.isLinked ? "Linked to Bus" : "Not Linked to Bus"}
-                  </Text>
-                </View>
-              </View>
-              <View className="flex-col gap-2 border-t border-gray-200 pt-2">
-                <Text className="text-md text-grayText">Home:</Text>
-                <View className="flex-row items-center justify-between gap-2">
-                  <View className="flex-row gap-2">
-                    <Image
-                      source={icons.homeLocationIcon}
-                      style={{ height: 16, width: 16 }}
-                    />
-                    <Text className="text-sm text-grayText">
-                      {child.homeLocation.address}
+              {/* Wrap the two Views in a single View */}
+              <View>
+                <View className="flex-row items-center gap-4">
+                  <Image
+                    source={child.image}
+                    style={{ height: 48, width: 48, borderRadius: 24 }}
+                  />
+                  <View className="flex-1">
+                    <Text className="text-lg font-semibold">{child.name}</Text>
+                    <Text className="text-sm text-blue-700 font-light">
+                      {child.isLinked ? "Linked to Bus" : "Not Linked to Bus"}
                     </Text>
                   </View>
-
-                  <View className="flex-row gap-2 ">
-                    <Text className="text-grayText">{`${child.homeLocation.latitude}"N`}</Text>
-                    <Text className="text-grayText">{`${child.homeLocation.longitude}"E`}</Text>
-                  </View>
                 </View>
-                <Text className="text-md text-grayText">School:</Text>
-                <View className="flex-row items-center justify-between gap-2">
-                  <View className="flex-row gap-2">
-                    <Image
-                      source={icons.schoolLocationIcon}
-                      style={{ height: 16, width: 16 }}
-                    />
-                    <Text className="text-sm text-grayText">
-                      {child.schoolLocation.address}
-                    </Text>
-                  </View>
+                <View className="flex-col gap-2 border-t border-gray-200 pt-2">
+                  <Text className="text-md text-grayText">Home:</Text>
+                  <View className="flex-row items-center justify-between gap-2">
+                    <View className="flex-row gap-2">
+                      <Image
+                        source={icons.homeLocationIcon}
+                        style={{ height: 16, width: 16 }}
+                      />
+                      <Text className="text-sm text-grayText">
+                        {child.homeLocation.address}
+                      </Text>
+                    </View>
 
-                  <View className="flex-row gap-2 ">
-                    <Text className="text-grayText">{`${child.schoolLocation.latitude}"N`}</Text>
-                    <Text className="text-grayText">{`${child.schoolLocation.longitude}"E`}</Text>
+                    <View className="flex-row gap-2 ">
+                      <Text className="text-grayText">{`${child.homeLocation.latitude}"N`}</Text>
+                      <Text className="text-grayText">{`${child.homeLocation.longitude}"E`}</Text>
+                    </View>
+                  </View>
+                  <Text className="text-md text-grayText">School:</Text>
+                  <View className="flex-row items-center justify-between gap-2">
+                    <View className="flex-row gap-2">
+                      <Image
+                        source={icons.schoolLocationIcon}
+                        style={{ height: 16, width: 16 }}
+                      />
+                      <Text className="text-sm text-grayText">
+                        {child.schoolLocation.address}
+                      </Text>
+                    </View>
+
+                    <View className="flex-row gap-2 ">
+                      <Text className="text-grayText">{`${child.schoolLocation.latitude}"N`}</Text>
+                      <Text className="text-grayText">{`${child.schoolLocation.longitude}"E`}</Text>
+                    </View>
                   </View>
                 </View>
               </View>
             </TouchableOpacity>
           ))}
         </View>
-        {/* form. input with date, predifined messages  */}
-        <View className="mt-6"></View>
+
+        {/* Date Picker Section */}
+        <View className="mt-6">
+          <Text className="text-lg font-normal mb-2">Choose Date</Text>
+          <TouchableOpacity
+            onPress={showDatepicker}
+            className="bg-white p-4 rounded-full shadow justify-between flex-row items-center gap-2"
+            style={{ height: 60 }} // Give it a fixed height for better touch area
+          >
+            <Text className="text-base text-grayText">
+              {date.toLocaleDateString()} {/* Display selected date */}
+            </Text>
+            <Text className="text-base text-grayText">Select a date</Text>
+          </TouchableOpacity>
+
+          {showPicker && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode="date"
+              display={Platform.OS === "ios" ? "spinner" : "default"} // Use 'spinner' for iOS, 'default' for Android
+              onChange={onChange}
+              minimumDate={new Date()}
+              maximumDate={
+                new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+              }
+            />
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
